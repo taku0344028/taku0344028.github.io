b6b6ef424b55aa7c3753614b6ac73d3444818b1f
Takuya Okubo <taku0344028@gmail.com>
2015-03-15 03:36:39 +0900

    ボード上を指で線引きする

--- board.js
+++ board.js
@@ -3,26 +3,53 @@
 	this.elm = elm;
 	var size = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
 	this.resize(size, size);
+	this.isSp = getDevice() == "sp";
     };
     Board.prototype = {
 	resize: function(w, h){
-	    this.elm.style.width  = w + "px";
-	    this.elm.style.height = h + "px";
+	    this.elm.setAttribute("width", w + "px");
+	    this.elm.setAttribute("height", h + "px");
+	    console.log("w = " + w + ", h = " + h);
 	},
 	touchStart: function(event){
 	    event.preventDefault();
+	    this.touched = true;
 	},
 	touchMove: function(event){
-	    var x = event.changedTouches[0].pageX;
-	    var y = event.changedTouches[0].pageY;
+	    if(!this.touched) return;
+	    var x = this.isSp ? event.changedTouches[0].pageX : event.pageX;
+	    var y = this.isSp ? event.changedTouches[0].pageY : event.pageY;
+	    var ctx = this.getContext("2d");
 	    document.getElementById("debug").innerHTML = "x = " + x + ", y = " + y;
+	    ctx.beginPath();
+	    ctx.fillRect(x, y, 10, 10);
+	},
+	touchEnd: function(event){
+	    this.touched = false;
 	}
     };
-    
+
+    /* デバイスの種類を調べる */
+    var getDevice = function(){
+	var ua = navigator.userAgent;
+	if(ua.indexOf("iPhone") > 0 || ua.indexOf("Android") > 0){
+	    return "sp";
+	}
+	else{
+	    return "other";
+	}
+    };
+
     window.onload = function(){
 	var mnCvs = document.getElementById("mainBoard");
 	var mnBrd = new Board(mnCvs);
-	mnCvs.addEventListener("touchstart", mnBrd.touchStart, false);
-	mnCvs.addEventListener("touchmove", mnBrd.touchMove, false);
+	var dType = getDevice();
+	var startEvent = dType == "sp" ? "touchstart" : "mousedown";
+	var moveEvent = dType == "sp" ? "touchmove" : "mousemove";
+	var endEvent = dType == "sp" ? "touchend" : "mouseup";
+	mnCvs.addEventListener(startEvent, mnBrd.touchStart, false);
+	mnCvs.addEventListener(moveEvent, mnBrd.touchMove, false);
+	mnCvs.addEventListener(endEvent, mnBrd.touchEnd, false);
+	document.getElementById("debug").innerHTML = "device = " + dType;
     };
 })();
