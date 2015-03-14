eb7661386d510a7e16f532ea9be14a5730e9d5a4
Takuya Okubo <taku0344028@gmail.com>
2015-03-15 02:54:12 +0900

    debug and touchmove event
    touchmoveイベントでdebugに位置情報を表示させる

--- board.js
+++ board.js
@@ -10,8 +10,12 @@
 	    this.elm.style.height = h + "px";
 	},
 	touchStart: function(event){
-	    console.log(event.changedTouches[0].pageX + ", " + event.changedTouches[0].pageY);
 	    event.preventDefault();
+	},
+	touchMove: function(event){
+	    var x = event.changedTouches[0].pageX;
+	    var y = event.changedTouches[0].pageY;
+	    document.getElementById("debug").innerHTML = "x = " + x + ", y = " + y;
 	}
     };
     
@@ -19,5 +23,6 @@
 	var mnCvs = document.getElementById("mainBoard");
 	var mnBrd = new Board(mnCvs);
 	mnCvs.addEventListener("touchstart", mnBrd.touchStart, false);
+	mnCvs.addEventListener("touchmove", mnBrd.touchMove, false);
     };
 })();
