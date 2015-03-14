d809e6a38ccc3b3656d4440b72ecfde4f9fafdb3
Takuya Okubo <taku0344028@gmail.com>
2015-03-15 02:42:44 +0900

    タッチイベントを取得

--- board.js
+++ board.js
@@ -1,23 +1,23 @@
 (function(){
-    var Board = function(id){
-	this.elm = document.getElementById(id);
+    var Board = function(elm){
+	this.elm = elm;
 	var size = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
 	this.resize(size, size);
-	this.setTouchEventListener();
     };
     Board.prototype = {
 	resize: function(w, h){
 	    this.elm.style.width  = w + "px";
 	    this.elm.style.height = h + "px";
 	},
-	setTouchEventListener: function(){
-	    this.elm.ontouchmove(function(event){
-		event.preventDefault();
-	    });
+	touchStart: function(event){
+	    console.log(event.changedTouches[0].pageX + ", " + event.changedTouches[0].pageY);
+	    event.preventDefault();
 	}
     };
     
     window.onload = function(){
-	var mnBrd = new Board("mainBoard");
+	var mnCvs = document.getElementById("mainBoard");
+	var mnBrd = new Board(mnCvs);
+	mnCvs.addEventListener("touchstart", mnBrd.touchStart, false);
     };
 })();
