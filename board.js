30761a07c5f38486db52b45c56ef431272110d9e
Takuya Okubo <taku0344028@gmail.com>
2015-03-14 23:33:03 +0900

    touchmove event

--- board.js
+++ board.js
@@ -1,18 +1,23 @@
 (function(){
-  var Board = function(id){
-    this.elm = document.getElementById(id);
-    var size = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
-    this.resize(size, size);
-  };
-  Board.prototype = {
-    resize: function(w, h){
-      this.elm.style.width = w + "px";
-      this.elm.style.height = h + "px";
-      console.log("width = " + w + ", height = " + h);
-    }
-  };
-  
-  window.onload = function(){
-    var mnBrd = document.getElementById("mainBoard");
-  };
+    var Board = function(id){
+	this.elm = document.getElementById(id);
+	var size = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
+	this.resize(size, size);
+	this.setTouchEventListener();
+    };
+    Board.prototype = {
+	resize: function(w, h){
+	    this.elm.style.width  = w + "px";
+	    this.elm.style.height = h + "px";
+	},
+	setTouchEventListener: function(){
+	    this.elm.ontouchmove(function(event){
+		event.preventDefault();
+	    });
+	}
+    };
+    
+    window.onload = function(){
+	var mnBrd = new Board("mainBoard");
+    };
 })();
